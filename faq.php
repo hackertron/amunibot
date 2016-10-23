ea80df67910161d510bc67ef0227687ceb876ee8
jay <cooljay.gupta@gmail.com>
2016-10-23 20:02:39 +0530

    faq test

--- faq.php
+++ faq.php
@@ -8,7 +8,7 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT * FROM faq");
+/*$result = pg_query($pg_conn, "SELECT * FROM faq");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
@@ -19,4 +19,19 @@ if (!pg_num_rows($result)) {
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 print "\n";
+
+*/
+function get_ans($query)
+{
+	$result = pg_query($pg_conn,"SELECT ans FROM faq where ques LIKE '%$query%'");
+	if (!$result)
+	{
+		return "no result found";
+	}
+	else
+	{
+		$ans = pg_fetch_assoc($result);
+		return $ans['ans'];
+	}
+}
 ?>
