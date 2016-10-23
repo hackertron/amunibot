6ca7b7af14d4dc4095d0392a2396a08e48e910a3
jay <cooljay.gupta@gmail.com>
2016-10-23 19:25:44 +0530

    faq test

--- faq.php
+++ faq.php
@@ -8,7 +8,7 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT ans FROM ques WHERE id = 1 ");
+$result = pg_query($pg_conn, "SELECT * FROM faq WHERE id = 1 ");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
