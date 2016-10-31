82c5e7e280e83681e94239868dc9df2b6d4b31d4
jay <cooljay.gupta@gmail.com>
2016-10-31 20:26:22 +0530

    change in faq

--- faq.php
+++ faq.php
@@ -6,7 +6,7 @@ function pg_connection_string_from_database_url() {
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 # Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
+#$pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 /*$result = pg_query($pg_conn, "SELECT * FROM faq");
 print "<pre>\n";
