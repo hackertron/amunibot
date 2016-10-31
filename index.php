b1c37f8a4ccb7b1aeefff0f1004e26bfeafe9bfd
jay <cooljay.gupta@gmail.com>
2016-10-31 21:48:10 +0530

    change in faq

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ try {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
 			$response = $client->sendMessage([
 					'chat_id' => $update->message->chat->id,
-					'text' => $update->message->text
+					'text' => print_r($ans)
 				]);
     }
     else
