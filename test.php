c42ef5bada7066535380318a7db6eb35986555a0
jay <cooljay.gupta@gmail.com>
2016-10-31 20:55:35 +0530

    change in faq

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+function get_ans($query)
+{
+	$pg_conn = pg_connect(pg_connection_string_from_database_url());
+	$result = pg_query($pg_conn,"SELECT ans FROM faq where ques LIKE '%$query%'");
+	if (!$result)
+	{
+		return "no result found";
+	}
+	else
+	{
+		$ans = pg_fetch_row($result);
+		echo "$ans";
+		
+	}
+}
+
+get_ans("dress code");
+
+?>
+
