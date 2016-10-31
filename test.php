107a5f588042f32e28163e176b491d50c4cc9685
jay <cooljay.gupta@gmail.com>
2016-10-31 22:11:12 +0530

    change in faq

--- test.php
+++ test.php
@@ -16,7 +16,7 @@ function get_ans($query)
 	else
 	{
 		$ans = pg_fetch_assoc($result);
-		echo $ans["ques"];
+		echo $ans["ans"];
 		#while ($ans = pg_fetch_row($result))
 		 #{ 
 		 #	print("$ans[0]\n");
